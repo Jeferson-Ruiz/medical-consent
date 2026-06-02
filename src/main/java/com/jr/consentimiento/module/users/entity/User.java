75c09b592c5f3440938de6ae16898e57f7697515
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-02 16:53:49 -0500

    feat: crear clase parcial de user

---
+++ src/main/java/com/jr/consentimiento/module/users/entity/User.java
@@ -0,0 +1,46 @@
+package com.jr.consentimiento.module.users.entity;
+
+import com.jr.consentimiento.shared.enums.TypeIdentification;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "usuarios")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "use_id", unique = true)
+    private Long id;
+
+    @Column(name = "use_nombre", nullable = false)
+    private String name;
+
+    @Column(name = "use_apellido", nullable = false)
+    private String lastName;
+
+    @Column(name = "use_tipo_id", nullable = false)
+    private TypeIdentification typeIdentification;
+
+    @Column(name = "use_tipo_id", nullable = false)
+    private String identificationNumber;
+
+    @Column(name = "use_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "use_contrasena",nullable = false)
+    private String password;
+}
