a77f40360fffa23565dcb38f2db16c07e816aafd
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-01 20:27:52 -0500

    feat: agregar variables a la interfaz para que logre generar del Pdf

--- src/main/java/com/jr/consentimiento/dto/ModelEmailDto.java
+++ src/main/java/com/jr/consentimiento/dto/ModelEmailDto.java
@@ -1,6 +1,7 @@
 
 package com.jr.consentimiento.dto;
 
+import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,4 +11,6 @@ public class ModelEmailDto {
     private String recipient;
     private String subject;
     private String message;
+    private String consentType;
+    private Map<String, String> data;
 }
