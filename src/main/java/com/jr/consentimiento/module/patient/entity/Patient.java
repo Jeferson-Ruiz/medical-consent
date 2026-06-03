5dc59b52121ceaacdacb403356d5e8ed74466df9
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-03 16:37:02 -0500

    feat: crear modulo de pacientes con entidad

---
+++ src/main/java/com/jr/consentimiento/module/patient/entity/Patient.java
@@ -0,0 +1,48 @@
+package com.jr.consentimiento.module.patient.entity;
+
+import org.springframework.data.annotation.Id;
+import com.jr.consentimiento.shared.enums.TypeIdentification;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "pacientes")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pac_id", unique = true)
+    private Long id;
+
+    @Column(name = "pac_nombre", nullable = false)
+    private String name;
+    
+    @Column(name = "pac_apellido", nullable = false)
+    private String lastName;
+
+    @Column(name = "pac_tipo_id", nullable = false)   
+    private TypeIdentification typeIdentification;
+
+    @Column(name = "pac_numero_id", nullable = false, unique = true)
+    private String numberIdentification;
+
+    @Column(name = "pac_telefono", nullable = false)
+    private String phone;
+
+    @Column(name = "pac_email", nullable = false)
+    private String email;
+
+    @Column(name = "pac_eps", nullable = false)
+    private String eps;
+}
