bce186b5ad54ebfa453b6d4fd105c0a57b348802
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-03 15:26:29 -0500

    feat: crear controlador temporal para la clase User

---
+++ src/main/java/com/jr/consentimiento/module/users/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.jr.consentimiento.module.users.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.jr.consentimiento.module.users.dto.RequestLogin;
+import com.jr.consentimiento.module.users.dto.UserRequestDto;
+import com.jr.consentimiento.module.users.dto.UserResponseDto;
+import com.jr.consentimiento.module.users.service.IUserService;
+
+@Controller
+@RestController("api/oauth")
+public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@RequestBody UserRequestDto requestDto){
+        UserResponseDto user =  userService.createUser(requestDto);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody RequestLogin request){
+        //temporal mientras implemento Jwt
+        return null;
+    }
+
+
+}
