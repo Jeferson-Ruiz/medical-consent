79bb573490e165cd8e26649cbed9a92388d405e8
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-04 09:45:16 -0500

    fix: corregir importacion del Id

--- src/main/java/com/jr/consentimiento/module/patient/entity/Patient.java
+++ src/main/java/com/jr/consentimiento/module/patient/entity/Patient.java
@@ -1,11 +1,11 @@
 package com.jr.consentimiento.module.patient.entity;
 
-import org.springframework.data.annotation.Id;
 import com.jr.consentimiento.shared.enums.TypeIdentification;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,11 +27,11 @@ public class Patient {
 
     @Column(name = "pac_nombre", nullable = false)
     private String name;
-    
+
     @Column(name = "pac_apellido", nullable = false)
     private String lastName;
 
-    @Column(name = "pac_tipo_id", nullable = false)   
+    @Column(name = "pac_tipo_id", nullable = false)
     private TypeIdentification typeIdentification;
 
     @Column(name = "pac_numero_id", nullable = false, unique = true)
@@ -45,4 +45,6 @@ public class Patient {
 
     @Column(name = "pac_eps", nullable = false)
     private String eps;
+
+    // mirar la manera de conectar con los Pdf
 }
