cfa23fc3b14b0f7d90450840d83da702fb0fa48a
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-04 09:46:06 -0500

    feat: crear metodos en el repositorio

--- src/main/java/com/jr/consentimiento/module/patient/repository/PatientRepository.java
+++ src/main/java/com/jr/consentimiento/module/patient/repository/PatientRepository.java
@@ -1,8 +1,12 @@
 package com.jr.consentimiento.module.patient.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jr.consentimiento.module.patient.entity.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    boolean existsByNumberIdentification(String number);
+
+    Optional<Patient> findByNumberIdentification(String number);
 }
